243b0cbc4ec40f4aeeea44f3c2f8433197205006
Nazarii Lishchynskiy <132710119+Nazarioo123@users.noreply.github.com>
2025-01-24 02:03:34 +0100

    Add files via upload

---
+++ CreateFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Authentication;
+using System.IO;
+using System.Text;
+
+namespace CreateFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string directoryPath = @"c:\HomeTask2";
+            string filePath = Path.Combine(directoryPath,"MyText.txt");
+
+            if (File.Exists(filePath))
+            {
+                Directory.CreateDirectory(directoryPath);
+                
+            }
+
+            Console.WriteLine("введіть текст");  
+            string txt = Console.ReadLine();
+
+            try
+            {
+                File.WriteAllText(filePath, txt);
+            }
+            catch (Exception ex)
+            { Console.WriteLine("помилкка"); }
+
+            File.WriteAllText("MyText.txt", txt);
+            Console.WriteLine("ваш текст записано ");
+
+           
+            string fileContent = File.ReadAllText(filePath);
+            Console.WriteLine("змістФайлу : ");
+            Console.WriteLine(fileContent);
+            
+            
+            
+         
+
+
+
+
+
+          
+        }
+    }
+}
